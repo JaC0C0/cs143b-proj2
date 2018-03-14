4eed337d39075ec0b6c1e5e3d27e77e803711a54
Dennis Tian <JaCoCo@localhost.localdomain>
2018-03-13 23:00:18 -0700

    Beginning implementation of file reader and cmd tokenizer

--- main.cpp
+++ main.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
 #include "Memory.h"
+#include "FileScanner.h"
 
 int main()
 {
     Memory virtualMemory;
+    FileScanner filescan;
+    filescan.processCmd();
     std::cout << "Holla" << std::endl;
     return 0;
 }
